b4b3a9a171e2fbc6bbe4ebc52cc719b50f9e6180
muguohong <714504851@qq.com>
2022-11-10 23:14:00 +0800

    initial project

---
+++ src/main/java/com/spm/eis/EisApplication.java
@@ -0,0 +1,13 @@
+package com.spm.eis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EisApplication.class, args);
+    }
+
+}
