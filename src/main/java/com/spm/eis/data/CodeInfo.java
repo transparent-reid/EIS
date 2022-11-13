e7631254c6c1a894ba58c6c5d8acf97762c79473
muguohong <714504851@qq.com>
2022-11-13 20:33:02 +0800

    add TableName("code_info")

--- src/main/java/com/spm/eis/data/CodeInfo.java
+++ src/main/java/com/spm/eis/data/CodeInfo.java
@@ -1,8 +1,10 @@
 package com.spm.eis.data;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("code_info")
 public class CodeInfo {
     private Long id;
     private String code;
--- src/main/java/com/spm/eis/mapper/CodeInfoMapper.java
+++ src/main/java/com/spm/eis/mapper/CodeInfoMapper.java
@@ -3,7 +3,6 @@ package com.spm.eis.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.spm.eis.data.CodeInfo;
 
-
 public interface CodeInfoMapper extends BaseMapper<CodeInfo> {
 
 }
