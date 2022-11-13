00cbf0bc2c1d5a6608eaa115e5b5497b1a0be9dc
muguohong <714504851@qq.com>
2022-11-13 18:20:01 +0800

    将数据库改为本地

--- src/main/resources/static/db/data-h2.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM code_info;
-
-INSERT INTO code_info (id, code, file_path) VALUES (1, '632626200206202105220204001010302001', 'filepath');
--- src/main/resources/static/db/schema-h2.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS code_info;
-
-CREATE TABLE code_info
-(
-    id       BIGINT NOT NULL,
-    code     CHAR(36) NULL DEFAULT NULL,
-    file_path VARCHAR(20) NULL DEFAULT NULL,
-    PRIMARY KEY (id)
-);
