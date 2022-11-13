f6af90c87747d446989912383013452f06f4020a
z17m <q1717qm@163.com>
2022-11-13 20:28:43 +0800

    mapper空指针异常解决

--- src/main/java/com/spm/eis/data/AddressCode.java
+++ src/main/java/com/spm/eis/data/AddressCode.java
@@ -1,11 +1,21 @@
 package com.spm.eis.data;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("address_code")
 public class AddressCode {
+
+    @TableId("code")
+    @TableField("code")
     private Long code;
+    @TableField("name")
     private String name;
+    @TableField("level")
     private int level;
+    @TableField("pcode")
     private Long pcode;
 }
